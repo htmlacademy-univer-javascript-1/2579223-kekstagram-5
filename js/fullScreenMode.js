9f9c477e3e34e4291217287223e6cb20f5bbb54a
Ksenia Egorenko <kseniaegorenko66949@gmail.com>
2024-12-25 19:27:26 +0700

    Исправлены замечания, добавлена загрузка комментариев порционно

--- js/fullScreenMode.js
+++ js/fullScreenMode.js
@@ -7,7 +7,6 @@ const bigPictureCloseElement = document.querySelector(".big-picture__cancel");
 const bigPictureImg = document.querySelector(".big-picture-photo");
 const postDescription = document.querySelector(".social__caption");
 const likesCount = document.querySelector(".likes-count");
-const commentsCount = document.querySelector(".comments-count");
 const socialCommentsCount = bigPictureModalElement.querySelector(
   ".social__comment-count"
 );
@@ -15,52 +14,72 @@ const socialCommentsLoader = bigPictureModalElement.querySelector(
   ".social__comments-loader"
 );
 const commentTemplate = document.querySelector("#post-comment").content;
-const fragment = document.createDocumentFragment();
+
+const COMMENTS_PER_LOAD = 5;
+let shownCommentsCount = 0;
+let currentPostComments = [];
+
+function renderComments() {
+  const commentsContainer = document.querySelector(".social__comments");
+  commentsContainer.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+
+  const commentsToShow = currentPostComments.slice(0, shownCommentsCount);
+  commentsToShow.forEach(({ avatar, message, name }) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    const commentImg = commentElement.querySelector(".social__picture");
+    const commentText = commentElement.querySelector(".social__text");
+
+    commentImg.src = avatar;
+    commentImg.alt = name;
+    commentText.textContent = message;
+
+    fragment.appendChild(commentElement);
+  });
+
+  commentsContainer.appendChild(fragment);
+
+  socialCommentsCount.innerHTML = `${shownCommentsCount} из <span class="comments-count">${currentPostComments.length}</span> комментариев`;
+
+  if (shownCommentsCount >= currentPostComments.length) {
+    socialCommentsLoader.classList.add("hidden");
+  } else {
+    socialCommentsLoader.classList.remove("hidden");
+  }
+}
 
 function showFullScreenPost(evt) {
   const target = evt.target;
   const pictureElement = target.closest(".picture");
 
-  function getPostsComments(element) {
-    const src = target.src;
-    const elementUrl = `http://localhost:3000/${element.url}`;
-    return elementUrl === src;
-  }
-
   if (pictureElement) {
     const likes = pictureElement.querySelector(".picture__likes").textContent;
-    const selectedPost = posts.find(getPostsComments);
-    const selectedPostComments = selectedPost.comments;
-    console.log(selectedPostComments);
+    currentPostComments = posts.find(
+      (post) => `http://localhost:3000/${post.url}` === target.src
+    ).comments;
 
-    const commentsContainer = document.querySelector(".social__comments");
-    commentsContainer.innerHTML = "";
+    shownCommentsCount = Math.min(
+      COMMENTS_PER_LOAD,
+      currentPostComments.length
+    );
 
     body.classList.add("modal-open");
     bigPictureModalElement.classList.remove("hidden");
-    socialCommentsCount.classList.add("hidden");
-    socialCommentsLoader.classList.add("hidden");
-
     bigPictureImg.src = target.getAttribute("src");
     postDescription.textContent = target.getAttribute("alt");
     likesCount.textContent = likes;
-    commentsCount.textContent = selectedPostComments.length;
 
-    selectedPostComments.forEach(({ avatar, message, name }) => {
-      const commentElement = commentTemplate.cloneNode(true);
-      const commentImg = commentElement.querySelector(".social__picture");
-      const commentText = commentElement.querySelector(".social__text");
-
-      commentImg.src = avatar;
-      commentImg.alt = name;
-      commentText.textContent = message;
-
-      fragment.appendChild(commentElement);
-    });
-
-    commentsContainer.appendChild(fragment);
+    renderComments();
   }
 }
+
+socialCommentsLoader.addEventListener("click", () => {
+  shownCommentsCount = Math.min(
+    shownCommentsCount + COMMENTS_PER_LOAD,
+    currentPostComments.length
+  );
+  renderComments();
+});
 
 bigPictureCloseElement.addEventListener("click", () => {
   bigPictureModalElement.classList.add("hidden");
